566634f75412755427634d4b7e384cd193caadc2
SimonKroll <32710261+SimonKroll@users.noreply.github.com>
2023-10-24 21:32:23 -0500

    Initial code commit

---
+++ web/js/main.js
@@ -0,0 +1,39 @@
+document.getElementById("left-arrow").addEventListener("click", ()=>{eel.get_random_name()}, false);
+//document.getElementById("button-number").addEventListener("click", ()=>{eel.get_random_number()}, false);
+//document.getElementById("button-date").addEventListener("click", ()=>{eel.get_date()}, false);
+//document.getElementById("button-ip").addEventListener("click", ()=>{eel.get_ip()}, false);
+
+
+var batteryInner = document.getElementsByClassName('batteryInner')[0];
+var batteryPercentage = 5;
+
+setInterval(function(){
+  if(batteryPercentage <= 100){
+    batteryInner.style.width = batteryPercentage + '%';
+    batteryPercentage = (batteryPercentage + 5) % 100 ;
+    document.getElementsByClassName("pct")[0].innerHTML = batteryPercentage;
+
+  }
+}, 200)
+
+
+var intervalId = window.setInterval(function(){
+  console.log("async called");
+ update_speed();
+}, 50);
+
+
+eel.expose(prompt_alerts);
+function prompt_alerts(description) {
+  alert(description);
+}
+
+async function update_speed() {
+  let value = await eel.get_random_number()();
+  value = (Math.round(value * 10) / 10).toFixed(1);
+  if(value<10.0){
+    value = "0"+String(value);
+  }
+ 
+  document.getElementById("speedometer").innerHTML = value;
+}
