08a0a9f47e6d7b23f0f355ee4a5d5085899fb9a9
Simon-PI <>
2023-10-28 21:47:00 -0500

    Added threading module for future use with webv ex

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import eel
+import threading
 import random
 from datetime import datetime
 
@@ -29,3 +30,25 @@ def get_ip():
 
 
 eel.start('index.html', mode='chrome', cmdline_args=['--kiosk'])
+
+
+"""
+def start_app():
+    eel_thread = threading.Thread(target=eel_start) # Eel app start.
+    eel_thread.setDaemon(True)
+    eel_thread.start() # Run eel in a seperate thread.
+
+    webview_start() # Start pywebview web browser.
+
+def eel_start():
+    # EEL app start.
+    eel.start("index.html", port=8000, mode=None,  shutdown_delay=0.0)
+
+def webview_start():
+    # pywebview start.
+    webview.create_window("App Name", "http://localhost:8000/index.html", fullscreen=True,frameless=True)
+    webview.start()
+  
+
+start_app() # Run app.
+"""
